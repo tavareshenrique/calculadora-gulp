93b5cfb8ee37236a8ad49b5b6907966647b8dbb3
Henrique Tavares <ihenrits@gmail.com>
2019-04-19 13:21:45 -0300

    Utilizando 'watch' no GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const { src, dest, parallel } = require("gulp");
-const watch = require("gulp-watch");
+//Utilizando Watch
+const gulp = require("gulp");
 const rename = require("gulp-rename");
 const minifyJS = require("gulp-uglify");
 const minifyCSS = require("gulp-uglifycss");
@@ -7,25 +7,48 @@ const sass = require("gulp-sass");
 const babel = require("gulp-babel");
 const cssImport = require("gulp-cssimport");
 
-function base() {
-  return src("src/templates/*.html").pipe(dest("public/"));
-}
+gulp.task(
+  "base",
+  gulp.series(function() {
+    return gulp.src("src/templates/*.html").pipe(gulp.dest("public/"));
+  })
+);
 
-function javascript() {
-  return src("src/js/*.js")
-    .pipe(babel({ presets: ["@babel/env"] }))
-    .pipe(minifyJS())
-    .pipe(rename({ extname: ".min.js" }))
-    .pipe(dest("public/assets/js/"));
-}
+gulp.task(
+  "javascript",
+  gulp.series(function() {
+    return gulp
+      .src("src/js/*.js")
+      .pipe(babel({ presets: ["@babel/env"] }))
+      .pipe(minifyJS())
+      .pipe(rename({ extname: ".min.js" }))
+      .pipe(gulp.dest("public/assets/js/"));
+  })
+);
 
-function css() {
-  return src("src/css/*.css", { ignoreInitial: false })
-    .pipe(cssImport())
-    .pipe(sass())
-    .pipe(minifyCSS())
-    .pipe(rename({ extname: ".min.css" }))
-    .pipe(dest("public/assets/css/"));
-}
+gulp.task(
+  "css",
+  gulp.series(function() {
+    return gulp
+      .src("src/css/*.css")
+      .pipe(cssImport())
+      .pipe(sass())
+      .pipe(minifyCSS())
+      .pipe(rename({ extname: ".min.css" }))
+      .pipe(gulp.dest("public/assets/css/"));
+  })
+);
 
-exports.default = parallel(base, javascript, css);
+gulp.task(
+  "watch",
+  gulp.series(function() {
+    gulp.watch("src/templates/*.html", gulp.parallel(["base"]));
+    gulp.watch("src/js/*.js", gulp.parallel(["javascript"]));
+    gulp.watch("src/css/*.css", gulp.parallel(["css"]));
+  })
+);
+
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("base", "javascript", "css", "watch"))
+);
